303fd06354fab32fd7cdc6e32dc40d6dc9d4985d
Kari Meghana <karimeghana02@gmail.com>
2025-09-06 14:58:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
 //your JS code here. If required.
-const table = document.querySelector('table');
-
-const nameCell = document.createElement('td');
-const ageCell = document.createElement('td');
-const branchCell = document.createElement('td');
-
-newRow.appendChild(nameCell);
-newRow.appendChild(ageCell);
-newRow.appendChild(branchCell);
-
